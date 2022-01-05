1cdab930cd3192aba4a53f29a2fc0ac6f4254987
ElieDahrouj <edarouj@hotmail.com>
2022-01-05 21:26:57 +0100

    fix : google site verification

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,6 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { name: 'format-detection', content: 'telephone=no' },
-      { name: 'google-site-verification', content: 'X9WmybXNQYmJlZM_nX3HCrGGMO-zZQp_UeIyPVK-b1Q' },
       {
         name: 'msapplication-TileColor',
         content: '#ffffff',
